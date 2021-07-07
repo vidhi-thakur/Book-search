2b9313d8390af39ad8946babff9afc1e2264b978
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-07-07 18:06:40 +0530

    added search functionality

--- book-search/src/App.js
+++ book-search/src/App.js
@@ -1,12 +1,62 @@
+import React, { useState } from 'react'
 import './App.css';
-import Header from './components/Header';
-import Body from './components/Body';
+import { BiSearchAlt } from 'react-icons/bi'
+import books from "./books.json"
+import Fuse from "fuse.js";
+import BookCard from "./components/BookCard"
 
 function App() {
+
+  const [data, setData] = useState(books)
+
+  const searchInput = (input) => {
+
+    if (!input) {
+      setData(books)
+    }
+    const fuse = new Fuse(data, {
+      keys: ["title", "author"]
+    });
+
+    console.log("fuse value >>>> ", fuse.keys)
+
+    let result = fuse.search(input);
+    const match = [];
+
+    console.log("input value >>>> " + input)
+    console.log("result value >>>> " + result)
+
+    if (!result.length) {
+      setData(books)
+    } else {
+      result.forEach(({ item }) => {
+        match.push(item)
+      })
+      setData(match)
+    }
+  };
+
+
   return (
     <div className="app">
-      <Header />
-      <Body />
+      {/* header */}
+
+      <div className="header">
+        <h1 className="header__title">Welcome to the library</h1>
+        <div className="header__searchfield">
+          <BiSearchAlt className="header__searchIcon" />
+          <input placeholder="Search Books" onChange={(e) => searchInput(e.target.value)} className="header__searchInput" />
+        </div>
+      </div>
+
+      {/* body */}
+      <div className="body">
+        {
+          data.map((item) => (
+            <BookCard {...item} key={item.title} />
+          ))
+        }
+      </div>
     </div>
   );
 }
--- book-search/src/components/BookCard.js
+++ book-search/src/components/BookCard.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./BookCard.css"
 
-function BookCard() {
+function BookCard({title, image, author, url}) {
     return (
         <div className="bookCard">
-            
+            <div>
+                <img className="bookCard__img" src={image} alt="book_image" />
+            </div>
+            <div className="bookCard__info">
+                <h4 className="m-1">{title}</h4>
+                <p className="m-1">{author}</p>
+                <a className="bookCard__link m-1" href={url}>Know more</a>
+            </div>
         </div>
     )
 }
